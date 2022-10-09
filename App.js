58fa58e1687067c80a9454da9357d5c34fa0a5a9
Арген Алимбаев <argenalimbaev@MacBook-Air-Argen.local>
2022-10-09 23:02:28 +0600

    finished todo app

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import TodoItem from "./screens/TodoItem";
+import { Button, Input } from "react-native-elements";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      _id: 1,
+      name: "CRISTIANAN SIIIIIUUUU",
+      isChecked: false,
+    },
+  ]);
+
+  const onPressNameHandler = () => {
+    setTodos((prev) => [
+      ...prev,
+      { _id: todos.length, name, isChecked: false },
+    ]);
+    setName("");
+  };
+
+  const toggleCheckedTodo = (idx) => {
+    const newArray = [].concat(todos);
+    newArray[idx].isChecked = !newArray[idx].isChecked;
+    setTodos(newArray);
+  };
+
+  const deleteTodo = (id) => {
+    const newArray = todos.filter((item) => item._id !== id);
+    setTodos(newArray);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading} h1>
+        ToDo 
+      </Text>
+
+      <View style={styles.actions}>
+        <Input
+          value={name}
+          style={styles.input}
+          onChangeText={setName}
+          placeholder="Name"
+        />
+        <Button
+          onPress={onPressNameHandler}
+          style={styles.button}
+          title="Add"
+          disabled={!name.length}
+        />
+      </View>
+
+      {todos &&
+        todos.map((todo, idx) => (
+          <TodoItem
+            idx={idx}
+            name={todo.name}
+            _id={todo._id}
+            isChecked={todo.isChecked}
+            toggleCheckedTodo={toggleCheckedTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
     </View>
   );
 }
@@ -13,8 +71,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e9eaee",
+    paddingTop: "13%",
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: "10%",
+  },
+  input: {
+    borderRadius: 15,
+    width: "70%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    border: "none",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    fontSize: 18,
+  },
+  button: {
+    fontSize: 18,
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+  actions: {
+    marginBottom: "5%",
   },
 });
---
+++ screens/TodoItem.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { CheckBox } from "react-native-elements";
+import { Button } from "@rneui/themed";
+
+const TodoItem = ({
+  idx,
+  name,
+  isChecked,
+  _id,
+  toggleCheckedTodo,
+  deleteTodo,
+}) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.checkbox}>
+        <CheckBox onPress={() => toggleCheckedTodo(idx)} checked={isChecked} />
+        <Text
+          style={
+            isChecked
+              ? {
+                  textDecorationLine: "line-through",
+                  color: "gray",
+                }
+              : null
+          }
+        >
+          {name}
+        </Text>
+      </View>
+      <Button onPress={() => deleteTodo(_id)} title={"Delete"} color="error" />
+    </View>
+  );
+};
+
+export default TodoItem;
+
+const styles = StyleSheet.create({
+  item: {
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: 12,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    marginHorizontal: "5%",
+    marginBottom: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  checkbox: {
+    flexDirection: "row",
+    alignItems: "center",
+    maxWidth: 160,
+  },
+  name: {
+    fontSize: 18,
+    marginLeft: 5,
+  },
+});
